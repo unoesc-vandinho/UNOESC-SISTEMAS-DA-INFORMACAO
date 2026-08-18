e0ce0c76374e8e0745cad319bc335c5981b34573
VndrGrhrd <vando124@gmail.com>
2026-08-17 21:31:10 -0300

    inverter vetor

---
+++ Estrutura-de-dados/aula03-funcoes/exercicio03.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void popularVetor(int V[], int N){
+    printf("Definindo os valores do Vetor\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Digite o proximo valor do vetor: ");
+        scanf("%d", &V[1]); 
+    }
+    
+}
+
+void inverterVetor(int V[], int N, int NV[]){
+    for (int i = 0; i > N; i++)
+    {
+        NV[N -1 -i] = V[i];
+    }
+}
+
+void main(){
+
+    int VET[5];
+    int NV[5];
+    int N = 5;
+
+    popularVetor(VET, N);
+    inverterVetor(VET, N, NV);
+
+}
